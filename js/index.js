7d28a5c5cf2dc56fe63e487348b86783ed950321
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-07 20:47:10 +0200

    startat med fetch av informationen från harry-potter databasen

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-import { userIsLoggedOut } from "./global.js";
+import { userIsLoggedOut, harryPotter_URL } from "./global.js";
 
 userIsLoggedOut()
+
+let allCharacters;
+
+const fetchHarryData = async()=>{
+ try{
+    const res= await fetch(harryPotter_URL);
+    if(!res.ok){
+        throw new Error("Något blev fel hos databasen i fetchHarryData", res.status)
+    }
+    const data=await res.json();
+    allCharacters=data;
+    console.log(allCharacters)
+
+ }catch (error){
+    console.error("Något blev fel med fetchning i fetchHarryData", error)
+ }
+}
+
+fetchHarryData()
