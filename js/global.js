7d6048c9d50a2365091bb510496116422d6b1f1b
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-07 14:06:27 +0200

    Gjort fram funktionen för att visa hur det ser ut i navigationen hvis du er utlogget. Skall exporteras från global.js till alla dokumenter

---
+++ js/global.js
@@ -0,0 +1,22 @@
+const userIsLoggedOut = ()=>{
+    const navigationList=document.querySelector("#navigationList");
+    navigationList.innerHTML="";
+
+    const list1=document.createElement("li");
+    const register=document.createElement("a");
+    register.href="./register.html";
+    register.classList.add("navigation-link");
+    register.classList.add("register");
+    register.innerHTML="Registrera";
+    list1.appendChild(register);
+    navigationList.appendChild(list1);
+
+    const list2=document.createElement("li");
+    const login=document.createElement("a");
+    login.href="./login.html";
+    login.classList.add("navigation-link");
+    login.innerHTML="Logga in";
+    list2.appendChild(login);
+    navigationList.appendChild(list2)
+}
+export {userIsLoggedOut}
