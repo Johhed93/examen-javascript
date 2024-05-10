45e58f977d167b9d887381a5e6ad4f7c7c8f0ff9
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-10 17:56:47 +0200

    startat på hvis ikke passordet stämmer

--- js/register.js
+++ js/register.js
@@ -1,2 +1,22 @@
 import { userIsLoggedOut } from "./global.js";
 userIsLoggedOut();
+
+const registerUser = async ()=> {
+ const fName=document.querySelector("#fNameInput").value;
+ const lName= document.querySelector("#lNameInput").value;
+ const username=document.querySelector("#usernameInput").value.toLowerCase();
+ const password=document.querySelector("#passwordInput").value;
+ const repeatPassword=document.querySelector("#repeatPasswordInput").value;
+ const errorMsg=document.querySelector("#errorMsg");
+ if(password!==repeatPassword){
+    errorMsg.innerHTML="Lösenordet matchar inte"
+    setTimeout(() => {
+        return errorMsg.innerHTML=""
+    }, 3000);
+ }
+ 
+
+
+}
+const submitFormBtn=document.querySelector("#submitFormBtn");
+submitFormBtn.addEventListener("click", registerUser)
