f8aa8a87cfe1c54fe2b37a887f8e143436f5af1d
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-12 19:56:47 +0200

    la till funktion för att hämta nuvarande år. Fortsatt på information som skall visas

--- js/character.js
+++ js/character.js
@@ -11,6 +11,7 @@ const fetchData= async()=> {
         }
         const data=await res.json();
         console.log(data[0])
+        showCharacter(data[0])
     }
    
     catch(error){
@@ -18,9 +19,17 @@ const fetchData= async()=> {
     }
 }
 fetchData();
+const getYear= ()=>{
+    const date=new Date();
+    return date.getFullYear();
+}
+getYear()
+
 const showCharacter = (character)=> {
+    const characterContainer= document.querySelector("#characterContainer")
     const container = document.createElement("div");
   container.classList.add("character-box");
+ 
 
   const characterName = document.createElement("h3");
   characterName.classList.add("character-name");
@@ -47,11 +56,29 @@ const showCharacter = (character)=> {
 
   const list = document.createElement("ul");
   list.classList.add("list");
+  
+  if(character.alternate_names.length>0){
+    const alternativeNames= document.createElement("li");
+    if(character.alternate_names.length>3){
+     const threeNames= character.alternate_names.splice(0,3).join(" ,")
+    alternativeNames.innerHTML=`Smeknamn: ${threeNames}`
+    }else{
+       const allNames= character.alternate_names.join(" ,")
+        alternativeNames.innerHTML=`Smeknamn: ${allNames}`
+    }
+    list.appendChild(alternativeNames)
+  }
   const house = document.createElement("li");
   textContainer.appendChild(list);
-
   house.innerHTML = `Hus: ${character.house}`;
   list.appendChild(house);
+  if (character.house === "") {
+    container.classList.add("unknown");
+    house.innerHTML=`Hus: Okänt`
+  } else {
+    container.classList.add(`${character.house}`)
+  }
+ 
   const gender = document.createElement("li");
   gender.innerHTML = `Kön: ${character.gender}`;
   list.appendChild(gender);
@@ -73,5 +100,18 @@ const showCharacter = (character)=> {
     alive.innerHTML = `Levande: Nej`;
   }
   list.appendChild(alive);
-  characterList.appendChild(container);
+  const secondList= document.createElement("ul");
+  secondList.classList.add("list");
+  secondRow.appendChild(secondList);
+
+  const age= document.createElement("li");
+  age.innerHTML=`Ålder: ${getYear()-character.yearOfBirth} år`;
+  secondList.appendChild(age);
+
+  const actor=document.createElement("li");
+  actor.innerHTML=`Skådespelare: ${character.actor}`
+  secondList.appendChild(actor)
+
+
+  characterContainer.appendChild(container);
 }
