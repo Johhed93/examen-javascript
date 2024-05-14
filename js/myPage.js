3b59cb1cfdfcfd785bb7f87f8ff46bb7cf260902
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-14 11:05:01 +0200

    startat med funktioner för att vise frem myPage

--- js/myPage.js
+++ js/myPage.js
@@ -1,2 +1,29 @@
-import { getHeaders, setLoggedInUser, database_url, displayError, checkIfLoggedIn, seePassword} from "./global.js";
+import { getHeaders, setLoggedInUser, database_url, displayError, checkIfLoggedIn, seePassword, getLoggedInUser} from "./global.js";
 checkIfLoggedIn()
+const myPageContainer=document.querySelector("#myPageContainer")
+const fetchUser= async()=>{
+    try{
+    const res= await fetch(`${database_url}/${getLoggedInUser()}`,{
+    method:"GET",
+    headers:getHeaders()
+    })
+    const data=await res.json();
+    console.log(data)
+    }catch(error){
+        console.error("Feil i henting av bruker", error)
+    }
+}
+fetchUser()
+
+const paragraphToInput =(p, container, user)=>{
+    let input=document.createElement("input");
+    input.value=user.name;
+    container.appendChild(input);
+    p.remove();
+}
+const inputToParagrph = (input, container, user)=>{
+    let p=document.createElement("p");
+    p.innerHTML=user.name;
+    container.appendChild(p);
+    input.remove();
+}
