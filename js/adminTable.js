b81ff7d2b50a1e1274ea6114011af1d78d30ffdd
Johhed93 <jhedlundbergqvist@gmail.com>
2024-05-15 18:16:07 +0200

    startat på sidan för admins

---
+++ js/adminTable.js
@@ -0,0 +1,10 @@
+import {
+    getHeaders,
+    database_url,
+    checkIfLoggedIn,
+    seePassword,
+    getLoggedInUser,
+    removeFromFavourties,
+    verifyUsername,
+  } from "./global.js";
+  checkIfLoggedIn();
